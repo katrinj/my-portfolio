12e50ecb7610d7bc9ffaae456d9d12fb34186ebc
katrinj <katrin.jets@gmail.com>
2024-12-27 17:29:24 +0200

    Added gallery carousel component.

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
@@ -7,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
+  index = signal(0);
 
+  pics = [
+    {url: "./img1_1920x1080px.jpg", caption: "Väike viik", alt: "Pic 1"},
+    {url: "./img2_1920x1080px.jpg", caption: "Sügisene kartulipõld", alt: "Pic 2"},
+    {url: "./img3_1920x1080px.jpg", caption: "Sinisem väike viik", alt: "Pic 3"},
+    {url: "./img4_1920x1080px.jpg", caption: "Männimetsa samblavaip", alt: "Pic 4"},
+    {url: "./img5_1920x1080px.jpg", caption: "Talvine laht", alt: "Pic 5"},
+    {url: "./img6_1920x1080px.jpg", caption: "Männimetsa taimestik", alt: "Pic 6"},
+    {url: "./img7_1920x1080px.jpg", caption: "Rabaäärne mets", alt: "Pic 7"},
+    {url: "./img8_1920x1080px.jpg", caption: "Ploomiõied", alt: "Pic 8"},
+    {url: "./img9_1920x1080px.jpg", caption: "Pohlad", alt: "Pic 9"},
+    {url: "./img10_1920x1080px.jpg", caption: "Kevadised krookused", alt: "Pic 10"}
+  ]
+
+  getCurrentPicUrl() {
+    return this.pics[this.index()].url;
+  }
+
+  getCurrentPicCaption() {
+    return this.pics[this.index()].caption;
+  }
+
+  getCurrentPicAlt() {
+    return this.pics[this.index()].alt;
+  }
+
+  isLastItemReached() {
+    return this.index() == this.pics.length - 1;
+  }
+
+  isFirstItemReached() {
+    return this.index() == 0;
+  }
+
+  nextPic() {
+    this.index.update(a => a + 1);
+  }
+
+  previousPic() {
+    this.index.update(a => a - 1);
+  }
+
+  activatePic(id : number) {
+    this.index.set(id);
+  }
 }
