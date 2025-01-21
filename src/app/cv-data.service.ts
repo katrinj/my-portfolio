d97f307ed7f24667a8bd9b3eb7787db0bdd70b89
katrinj <katrin.jets@gmail.com>
2025-01-21 18:22:59 +0200

    Updated cv page. Added service for keeping cv related data.

---
+++ src/app/cv-data.service.ts
@@ -0,0 +1,272 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CvDataService {
+
+  constructor() { }
+
+  private timeLineData_et = [
+    {
+    year: "2023 - 2024",
+    company: "Webhelp OÜ / Concentrix Estonia OÜ",
+    role: "Andmesisestaja",
+    remark: "täiskohaga kaugtöö",
+    type: "work"
+    },
+    {
+      year: "2022 - 2023",
+      company: "Tartu Kunstikool",
+      role: "Koolitaja",
+      subject: "3D kursused algajatele",
+      remark: "õpilaste ja täiskasvanute rühmadele",
+      type: "work"
+    },    
+    {
+      year: "2020 - 2023",
+      company: "Tartu Kunstikool",
+      role: "3D-kunstnik-kujundaja", 
+      remark: "4. taseme kutsetunnistus",
+      type: "school"
+    },
+    {
+      year: "2018 - 2021",
+      company: "Luisa Tõlkebüroo OÜ",
+      role: "Tõlkija",
+      subject: "ENG → EST", 
+      remark: "projektipõhine kaugtöö",
+      type: "work"
+    },
+    {
+      year: "2017 - 2020",
+      company: "Tartu Ülikool",
+      role: "Tõlkeõpetus", 
+      subject: "ENG ↔ EST; GER → EST",
+      remark: "MA (cum laude)",
+      type: "school"
+    },
+    {
+      year: "2015 - 2020",
+      company: "Playtech Estonia OÜ",
+      role: "Tarkvaraarendaja",
+      subject: "Java", 
+      remark: "vahelduva koormusega töö kontoris",
+      type: "work"
+    },
+    {
+      year: "2008 - 2013",
+      company: "Tartu Ülikool",
+      role: "Informaatika",
+      subject: "Keeletehnoloogia",
+      remark: "MSc",
+      type: "school"
+    },
+    {
+      year: "2007 - 2008",
+      company: "Aktors OÜ",
+      role: "Tarkvaraarendaja",
+      subject: "Java", 
+      remark: "poole koormusega töö enamasti kontoris",
+      type: "work"
+    },
+    {
+      year: "2004 - 2008",
+      company: "Tartu Ülikool",
+      role: "Infotehnoloogia",
+      remark: "BSc",
+      type: "school"
+    }
+  ];
+
+  private timeLineData_en = [
+    {
+    year: "2023 - 2024",
+    company: "Webhelp OÜ / Concentrix Estonia OÜ",
+    role: "Data entry specialist",
+    remark: "fulltime, remote",
+    type: "work"
+    },
+    {
+      year: "2022 - 2023",
+      company: "Tartu Kunstikool",
+      role: "Instructor",
+      subject: "3D courses for beginners",
+      remark: "highschool student and adult groups",
+      type: "work"
+    },
+    {
+      year: "2020 - 2023",
+      company: "Tartu Kunstikool",
+      role: "3D-kunstnik-kujundaja", 
+      remark: "4. taseme kutsetunnistus",
+      type: "school"
+    },
+    {
+      year: "2018 - 2021",
+      company: "Luisa Tõlkebüroo OÜ",
+      role: "Tõlkija",
+      subject: "ENG → EST", 
+      remark: "projektipõhine kaugtöö",
+      type: "work"
+    },
+    {
+      year: "2017 - 2020",
+      company: "Tartu University",
+      role: "Translation studies", 
+      subject: "ENG ↔ EST; GER → EST",
+      remark: "MA (cum laude)",
+      type: "school"
+    },
+    {
+      year: "2015 - 2020",
+      company: "Playtech Estonia OÜ",
+      role: "Software developer",
+      subject: "Java", 
+      remark: "",
+      type: "work"
+    },
+    {
+      year: "2008 - 2013",
+      company: "Tartu University",
+      role: "Computer science",
+      subject: "Language technology",
+      remark: "MSc",
+      type: "school"
+    },
+    {
+      year: "2007 - 2008",
+      company: "Aktors OÜ",
+      role: "Software developer",
+      subject: "Java", 
+      remark: "",
+      type: "work"
+    },
+    {
+      year: "2004 - 2008",
+      company: "Tartu University",
+      role: "Information technology",
+      remark: "BSc",
+      type: "school"
+    }
+  ];
+
+  private programmingLanguages = ["Java", "JS", "TS", "HTML", "CSS", "SQL"];
+  private helperSoftware = ["Angular", "Spring", "Docker", "Jenkins", "Git"];
+  private artSoftware = ["Blender", "Krita", "Adobe Photoshop", "Adobe Illustrator"];
+
+  private languages_et = [
+    {
+      name: "Eesti keel",
+      state: "aktiivne",
+      description: "Emakeel"
+    },
+    {
+      name: "Inglise keel",
+      state: "aktiivne",
+      studies: "Õppinud üle 11 aasta",
+      description: "Igapäevases kasutuses nii kõnes kui kirjas"
+    },
+    {
+      name: "Saksa keel",
+      state: "passiivne",
+      studies: "Õppinud 8 aastat",
+      description: "Võimeline teataval määral aru saama ja rääkima",
+    },
+    {
+      name: "Jaapani keel",
+      state: "passiivne",
+      studies: "Õppinud 6 aastat",
+      description: "Võimeline vähesel määral aru saama ja rääkima"
+    }
+  ];
+
+  languages_en = [
+    {
+      name: "Estonian",
+      state: "active",
+      description: "Native tongue"
+    },
+    {
+      name: "English",
+      state: "active",
+      studies: "Studied over 11 years",
+      description: "In daily use both speaking and writing"
+    },
+    {
+      name: "German",
+      state: "passive",
+      studies: "Studied 8 years",
+      description: "Can understand to some extent, speak a little"
+    },
+    {
+      name: "Japanese",
+      state: "passive",
+      studies: "Studied 6 years",
+      description: "Can understand and speak a little"
+    }
+  ];
+
+  private language_certificates = [
+    {
+      subject: "English",
+      type: "CEFR C1",
+      year: "2019"
+    },
+    {
+      subject: "Japanese",
+      type: "JLPT N3",
+      year: "2011"
+    }
+  ];
+
+  private tech_certificates = [
+    {
+      subject: "Java",
+      type: "SE 8 OCA",
+      year: "2016"
+    },
+    {
+      subject: "Java",
+      type: "SE 8 OCP",
+      year: "2017"
+    }
+  ];
+
+  getTimeLineItems(lang: string) {
+    if (lang == "en") {
+      return this.timeLineData_en;
+    } else {
+      return this.timeLineData_et;
+    }
+  }
+
+  getProgrammingLanguages(): string[] {
+    return this.programmingLanguages;
+  }
+
+  getHelperSoftware(): string[] {
+    return this.helperSoftware;
+  }
+
+  getArtSoftware(): string[] {
+    return this.artSoftware;
+  }
+
+  getLanguageItems(lang: string) {
+    if (lang == "en") {
+      return this.languages_en;
+    } else {
+      return this.languages_et;
+    }
+  }
+
+  getTechCertificates() {
+      return this.tech_certificates;
+  }
+
+  getLanguageCertificates() {
+    return this.language_certificates;
+  }
+
+}
--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, ViewChild } from '@angular/core';
+import { CvDataService } from '../cv-data.service';
+import { LanguageService } from '../language.service';
 
 @Component({
   selector: 'app-cv',
@@ -6,6 +8,96 @@ import { Component } from '@angular/core';
   templateUrl: './cv.component.html',
   styleUrl: './cv.component.css'
 })
-export class CvComponent {
+export class CvComponent implements AfterViewInit {
+
+  cvDataService: CvDataService = inject(CvDataService);
+  languageService: LanguageService = inject(LanguageService);
+
+  @ViewChild('canvas', {static: true}) myCanvas!: ElementRef;
+  ngAfterViewInit(): void {
+    const canvas: HTMLCanvasElement = this.myCanvas.nativeElement;
+    const context = canvas.getContext('2d');
+
+    let timeLineItems = document.querySelector(".timeLineItems") as HTMLDivElement;
+    let timeLineData = timeLineItems.getBoundingClientRect();
+    let timeLineHeight = timeLineData.height;
+    console.log("offset: " + timeLineItems.offsetHeight + " height: " + timeLineData.height);
+    
+    const extra = 80;    
+    canvas.height = timeLineHeight + extra;
+    canvas.width = 20;
+
+    if (context) {
+      this.drawTimeLine(context, 10, 10, timeLineHeight! + (extra - 10));
+    }
+  }
+
+  drawTimeLine(context : CanvasRenderingContext2D, x: number, y: number, h: number) {
+    context.strokeStyle = 'white';
+    context.fillStyle = 'white';
+    context.lineWidth = 6;
+
+    // timeline
+    this.drawTimeline(context, x, y, h);
+
+    // circles at both ends of the timeline
+    this.drawCirleAt(context, x, y, 10);
+    this.drawCirleAt(context, x, h, 10);
+  }
+
+  drawTimeline(context : CanvasRenderingContext2D, x: number, y: number, h: number) {
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(x, h);
+    context.stroke();
+  }
+
+  drawCirleAt(context : CanvasRenderingContext2D, x: number, y: number, radius: number) {
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI);
+    context.fill();
+  }
+
+  isWorkItem(type: string): boolean {
+    return type == "work";
+  }
+
+  getTimeLineItemTypeClass(type: string) {
+    if (this.isWorkItem(type)) {
+      return "timeLineItem timeLineItemWork"
+    } else {
+      return "timeLineItem timeLineItemSchool"
+    }
+  }
+
+  getTimeLineItems() {
+    const lang = this.languageService.getCurrentLanguage();
+    return this.cvDataService.getTimeLineItems(lang);
+  }
+
+  getProgrammingLanguages(): string[] {
+    return this.cvDataService.getProgrammingLanguages();
+  }
+
+  getHelperSoftware(): string[] {
+    return this.cvDataService.getHelperSoftware();
+  }
+
+  getArtSoftware(): string[] {
+    return this.cvDataService.getArtSoftware();
+  }
+
+  getLanguageItems() {
+    const lang = this.languageService.getCurrentLanguage();
+    return this.cvDataService.getLanguageItems(lang);
+  }
+
+  getTechCertificates() {
+    return this.cvDataService.getTechCertificates();
+  }
+
+  getLanguageCertificates() {
+    return this.cvDataService.getLanguageCertificates();
+  }
 
 }
